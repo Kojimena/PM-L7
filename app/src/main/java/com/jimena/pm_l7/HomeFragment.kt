5db7cd2b2396b8f446d0fdad617b49c17357b435
Jimena Hernández <77686378+Kojimena@users.noreply.github.com>
2022-08-27 16:30:20 -0600

    navGraph

--- app/src/main/java/com/jimena/pm_l7/HomeFragment.kt
+++ app/src/main/java/com/jimena/pm_l7/HomeFragment.kt
@@ -1,59 +1,39 @@
 package com.jimena.pm_l7
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.navArgs
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [HomeFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
-class HomeFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+class HomeFragment : Fragment(R.layout.fragment_home) {
+    private lateinit var textHome: TextView
+    private lateinit var buttonActualizar: TextView
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
+    private val args: HomeFragmentArgs by navArgs()
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        textHome = view.findViewById(R.id.actualizacion)
+        buttonActualizar = view.findViewById(R.id.actualizarperfil)
+
+        val email = args.email
+
+
+        textHome.text = "Hola $email, necesitamos que \n actualices tu perfil!"
+
+        setListeners()
+    }
+
+    private fun setListeners() {
+        buttonActualizar.setOnClickListener {
+            val action = HomeFragmentDirections.actionHomeFragmentToProfileFragment(
+                args.email
+            )
+            requireView().findNavController().navigate(action)
         }
-    }
-
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
-    }
-
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment HomeFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            HomeFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
     }
 }
